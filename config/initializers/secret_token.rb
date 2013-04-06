285218853b2a6fef3b017c1ae07a6fa03910a33c
nbirkes <nbirkes@gmail.com>
2013-04-05 20:53:13 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Five72::Application.config.secret_token = '7cde7142ef017a503ce6d96435efbb38a3dc0761671b77bed6ac6e4b9bb1999ee89b615ece63420693de7f128c46859ad4e08c9b77e85b7929ecdfa2af9654a7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Five72::Application.config.session_store :cookie_store, key: '_five72_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Five72::Application.config.session_store :active_record_store
